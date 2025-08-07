6985943d43c4014e35a2e2fe5be82a31f59451ce
michal-mm <michal@Mac.lan>
2025-08-07 14:38:20 +0200

    adding transaction to store data in the DB based on the messageId. If the same message comes again, exception is thrown and we will use non-retryable exception for handling

--- email-notification-service/src/main/java/com/michalmm/kafka/ws/emailnotification/handler/ProductCreatedEventHandler.java
+++ email-notification-service/src/main/java/com/michalmm/kafka/ws/emailnotification/handler/ProductCreatedEventHandler.java
@@ -2,6 +2,7 @@ package com.michalmm.kafka.ws.emailnotification.handler;
 
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
+import org.springframework.dao.DataIntegrityViolationException;
 import org.springframework.http.HttpMethod;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
@@ -11,6 +12,7 @@ import org.springframework.kafka.support.KafkaHeaders;
 import org.springframework.messaging.handler.annotation.Header;
 import org.springframework.messaging.handler.annotation.Payload;
 import org.springframework.stereotype.Component;
+import org.springframework.transaction.annotation.Transactional;
 import org.springframework.web.client.HttpServerErrorException;
 import org.springframework.web.client.ResourceAccessException;
 import org.springframework.web.client.RestTemplate;
@@ -18,6 +20,8 @@ import org.springframework.web.client.RestTemplate;
 import com.michalmm.kafka.ws.core.ProductCreatedEvent;
 import com.michalmm.kafka.ws.emailnotification.error.NotRetryableException;
 import com.michalmm.kafka.ws.emailnotification.error.RetryableException;
+import com.michalmm.kafka.ws.emailnotification.io.ProcessedEventEntity;
+import com.michalmm.kafka.ws.emailnotification.io.ProcessedEventRepository;
 
 @Component
 @KafkaListener(topics="product-created-events-topic")
@@ -27,13 +31,18 @@ public class ProductCreatedEventHandler {
 	
 	private RestTemplate restTemplate;
 	
+	private ProcessedEventRepository processedEventRepository;
 	
-	public ProductCreatedEventHandler(RestTemplate restTemplate) {
+	
+	public ProductCreatedEventHandler(RestTemplate restTemplate,
+				ProcessedEventRepository processedEventrepository) {
 		this.restTemplate = restTemplate;
+		this.processedEventRepository = processedEventrepository;
 	}
 	
 
 	@KafkaHandler
+	@Transactional
 	public void handle(@Payload ProductCreatedEvent productCreatedEvent,
 						@Header(value="messageId", required=true) String messageId, 
 						@Header(KafkaHeaders.RECEIVED_KEY) String messageKey) {
@@ -59,6 +68,14 @@ public class ProductCreatedEventHandler {
 			throw new NotRetryableException(ex);
 		} catch (Exception ex) {
 			LOGGER.error("General exception --> " + ex.getMessage());
+			throw new NotRetryableException(ex);
+		}
+		
+		// save unique message id in the DB
+		try {
+			processedEventRepository.save(new ProcessedEventEntity(messageId, productCreatedEvent.getProductId()));
+		} catch(DataIntegrityViolationException ex) {
+			LOGGER.error("Duplicate keys in DB, not processing again --> " + ex.getMessage());
 			throw new NotRetryableException(ex);
 		}
 	}
