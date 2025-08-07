2cbf85c0e8ec64c2c568ff9fc6eccd6b5096e7b7
michal-mm <michal@Mac.lan>
2025-08-07 11:06:15 +0200

    adding implementation to use Dead letter topic (queue)

--- email-notification-service/src/main/java/com/michalmm/kafka/ws/emailnotification/KafkaConsumerConfiguration.java
+++ email-notification-service/src/main/java/com/michalmm/kafka/ws/emailnotification/KafkaConsumerConfiguration.java
@@ -12,6 +12,9 @@ import org.springframework.core.env.Environment;
 import org.springframework.kafka.config.ConcurrentKafkaListenerContainerFactory;
 import org.springframework.kafka.core.ConsumerFactory;
 import org.springframework.kafka.core.DefaultKafkaConsumerFactory;
+import org.springframework.kafka.core.KafkaTemplate;
+import org.springframework.kafka.listener.DeadLetterPublishingRecoverer;
+import org.springframework.kafka.listener.DefaultErrorHandler;
 import org.springframework.kafka.support.serializer.ErrorHandlingDeserializer;
 import org.springframework.kafka.support.serializer.JsonDeserializer;
 
@@ -46,9 +49,14 @@ public class KafkaConsumerConfiguration {
 	
 	@Bean
 	ConcurrentKafkaListenerContainerFactory<String, Object> kafkaListenerContainerFactory(
-			ConsumerFactory<String, Object> consumerFactory) {
+			ConsumerFactory<String, Object> consumerFactory,
+			KafkaTemplate<String, Object> kafkaTemplate) {
+		
+		DefaultErrorHandler errorHandler = new DefaultErrorHandler(new DeadLetterPublishingRecoverer(kafkaTemplate));
+		
 		ConcurrentKafkaListenerContainerFactory<String, Object> factory = new ConcurrentKafkaListenerContainerFactory<>();
 		factory.setConsumerFactory(consumerFactory);
+		factory.setCommonErrorHandler(errorHandler);
 		
 		return factory;
 	}
