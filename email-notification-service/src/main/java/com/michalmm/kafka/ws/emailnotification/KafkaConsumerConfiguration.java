0da15d6298babb472c80ee5db1f7755db810ba79
michal-mm <michal@Mac.lan>
2025-08-07 11:24:52 +0200

    handling of dead letter topic

--- email-notification-service/src/main/java/com/michalmm/kafka/ws/emailnotification/KafkaConsumerConfiguration.java
+++ email-notification-service/src/main/java/com/michalmm/kafka/ws/emailnotification/KafkaConsumerConfiguration.java
@@ -4,7 +4,9 @@ import java.util.HashMap;
 import java.util.Map;
 
 import org.apache.kafka.clients.consumer.ConsumerConfig;
+import org.apache.kafka.clients.producer.ProducerConfig;
 import org.apache.kafka.common.serialization.StringDeserializer;
+import org.apache.kafka.common.serialization.StringSerializer;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
@@ -12,11 +14,14 @@ import org.springframework.core.env.Environment;
 import org.springframework.kafka.config.ConcurrentKafkaListenerContainerFactory;
 import org.springframework.kafka.core.ConsumerFactory;
 import org.springframework.kafka.core.DefaultKafkaConsumerFactory;
+import org.springframework.kafka.core.DefaultKafkaProducerFactory;
 import org.springframework.kafka.core.KafkaTemplate;
+import org.springframework.kafka.core.ProducerFactory;
 import org.springframework.kafka.listener.DeadLetterPublishingRecoverer;
 import org.springframework.kafka.listener.DefaultErrorHandler;
 import org.springframework.kafka.support.serializer.ErrorHandlingDeserializer;
 import org.springframework.kafka.support.serializer.JsonDeserializer;
+import org.springframework.kafka.support.serializer.JsonSerializer;
 
 
 
@@ -59,5 +64,21 @@ public class KafkaConsumerConfiguration {
 		factory.setCommonErrorHandler(errorHandler);
 		
 		return factory;
+	}
+	
+	@Bean
+	KafkaTemplate<String, Object> kafkaTempalte(ProducerFactory<String, Object> producerFactory) {
+		return new KafkaTemplate<>(producerFactory);
+	}
+	
+	@Bean
+	ProducerFactory<String, Object> producerFactory() {
+		Map<String, Object> config = new HashMap<>();
+		config.put(ProducerConfig.BOOTSTRAP_SERVERS_CONFIG, 
+				environment.getProperty("spring.kafka.producer.bootstrap-servers"));
+		config.put(ProducerConfig.VALUE_SERIALIZER_CLASS_CONFIG, JsonSerializer.class);
+		config.put(ProducerConfig.KEY_SERIALIZER_CLASS_CONFIG, StringSerializer.class);
+		
+		return new DefaultKafkaProducerFactory<>(config);
 	}
 }
