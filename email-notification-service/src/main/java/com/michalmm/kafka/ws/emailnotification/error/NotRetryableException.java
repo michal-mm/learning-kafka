4b29c15e9560bce33fed27c1ef74d81250180d16
michal-mm <michal@Mac.lan>
2025-08-07 11:27:20 +0200

    basic stub implementation of retryable nad non-retryable exceptions

---
+++ email-notification-service/src/main/java/com/michalmm/kafka/ws/emailnotification/error/NotRetryableException.java
@@ -0,0 +1,16 @@
+package com.michalmm.kafka.ws.emailnotification.error;
+
+public class NotRetryableException extends RuntimeException{
+
+	public NotRetryableException(String message) {
+		super(message);
+		// TODO Auto-generated constructor stub
+	}
+
+	public NotRetryableException(Throwable cause) {
+		super(cause);
+		// TODO Auto-generated constructor stub
+	}
+
+	
+}
