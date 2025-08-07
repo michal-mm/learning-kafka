9f71e30de4a678012519176269e39d044df39710
michal-mm <michal@Mac.lan>
2025-08-07 14:33:32 +0200

    Adding base repository implementation

---
+++ email-notification-service/src/main/java/com/michalmm/kafka/ws/emailnotification/io/ProcessedEventRepository.java
@@ -0,0 +1,9 @@
+package com.michalmm.kafka.ws.emailnotification.io;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface ProcessedEventRepository extends JpaRepository<ProcessedEventEntity, Long>{
+
+}
